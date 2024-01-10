2ac75fd0a088983a087f2f3e2d2dfe9839a8d982
mikesszinyei <szinyeimikes@gmail.com>
2024-01-10 10:32:10 +0100

    teszt7

--- src/main/java/com/example/Romb.java
+++ src/main/java/com/example/Romb.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Romb {
     public double calcArea(double side, double a, double b) {
-        return 0;
+        return Math.pow(side,2)*Math.sin(a);
     }
 
 }
