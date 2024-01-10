bc953c356ad2563bd0c5983d17d25145aee3621f
mikesszinyei <szinyeimikes@gmail.com>
2024-01-10 10:23:37 +0100

    teszt2

--- src/test/java/RombTest.java
+++ src/test/java/RombTest.java
@@ -1,5 +1,16 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+import com.example.Romb;
 
 public class RombTest {
     Romb romb;
+    @BeforeTest
+    public void setup() {
+        this.romb = new Romb();
+    }
+    @Test
+    public void testCalc() {
+
+    }
 }
